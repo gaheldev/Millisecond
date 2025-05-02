7ee3d5b60e3724826f3e49e5f489be6e34a2ca66
Gahel <music@gahel.fr>
2025-05-02 07:07:38 +0200

    Group diagnostics by categories

--- src/window.py
+++ src/window.py
@@ -27,9 +27,12 @@ class ProaudioSetupWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'ProaudioSetupWindow'
 
     main_box = Gtk.Template.Child()
-    explanations = Gtk.Template.Child()
     preferences_page = Gtk.Template.Child()
-    preferences_group = Gtk.Template.Child()
+    explanations_group = Gtk.Template.Child()
+    user_group = Gtk.Template.Child()
+    cpu_group = Gtk.Template.Child()
+    kernel_group = Gtk.Template.Child()
+    io_group = Gtk.Template.Child()
     diagnostic_row = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
@@ -43,58 +46,83 @@ class ProaudioSetupWindow(Adw.ApplicationWindow):
         self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.toolbar_view.set_content(self.main_box)
 
-        self.explanations = Gtk.Label()
-        self.explanations.set_text("coucou")
-        self.main_box.append(self.explanations)
-
         self.preferences_page = Adw.PreferencesPage()
 
-        self.preferences_group = Adw.PreferencesGroup()
-        self.preferences_group.set_title("Diagnostics")
+        # Explanations and disclaimers
+
+        self.explanations_group = Adw.PreferencesGroup()
+
+        self.explanations = Gtk.Label()
+        self.explanations.set_text("coucou")
+        self.explanations_group.add(self.explanations)
+
+        # User diagnostics
+
+        self.user_group = Adw.PreferencesGroup()
+        self.user_group.set_title("User")
 
         self.diagnostic_row = DiagnosticRow(self.rtcqs, "root")
-        self.preferences_group.add(self.diagnostic_row)
+        self.user_group.add(self.diagnostic_row)
 
         self.diagnostic_row = DiagnosticRow(self.rtcqs, "audio_group", "#audio_group")
-        self.preferences_group.add(self.diagnostic_row)
-
-        self.diagnostic_row = DiagnosticRow(self.rtcqs, "governor", "#cpu_frequency_scaling")
-        self.preferences_group.add(self.diagnostic_row)
-
-        self.diagnostic_row = DiagnosticRow(self.rtcqs, "smt", "#Simultaneous_threading")
-        self.preferences_group.add(self.diagnostic_row)
-
-        self.diagnostic_row = DiagnosticRow(self.rtcqs, "kernel_config")
-        self.preferences_group.add(self.diagnostic_row)
-
-        self.diagnostic_row = DiagnosticRow(self.rtcqs, "high_res_timers", "#installing_a_real-time_kernel")
-        self.preferences_group.add(self.diagnostic_row)
-
-        self.diagnostic_row = DiagnosticRow(self.rtcqs, "tickless", "#installing_a_real-time_kernel")
-        self.preferences_group.add(self.diagnostic_row)
-
-        self.diagnostic_row = DiagnosticRow(self.rtcqs, "preempt_rt", "#do_i_really_need_a_real-time_kernel")
-        self.preferences_group.add(self.diagnostic_row)
-
-        self.diagnostic_row = DiagnosticRow(self.rtcqs, "mitigations", "#disabling_spectre_and_meltdown_mitigations")
-        self.preferences_group.add(self.diagnostic_row)
+        self.user_group.add(self.diagnostic_row)
 
         self.diagnostic_row = DiagnosticRow(self.rtcqs, "rt_prio", "#limitsconfaudioconf")
-        self.preferences_group.add(self.diagnostic_row)
+        self.user_group.add(self.diagnostic_row)
 
-        self.diagnostic_row = DiagnosticRow(self.rtcqs, "swappiness", "#sysctlconf")
-        self.preferences_group.add(self.diagnostic_row)
+        # CPU diagnostics
 
-        self.diagnostic_row = DiagnosticRow(self.rtcqs, "filesystems", "#filesystems")
-        self.preferences_group.add(self.diagnostic_row)
+        self.cpu_group = Adw.PreferencesGroup()
+        self.cpu_group.set_title("CPU")
 
-        self.diagnostic_row = DiagnosticRow(self.rtcqs, "irqs")
-        self.preferences_group.add(self.diagnostic_row)
+        self.diagnostic_row = DiagnosticRow(self.rtcqs, "governor", "#cpu_frequency_scaling")
+        self.cpu_group.add(self.diagnostic_row)
+
+        self.diagnostic_row = DiagnosticRow(self.rtcqs, "smt", "#Simultaneous_threading")
+        self.cpu_group.add(self.diagnostic_row)
 
         self.diagnostic_row = DiagnosticRow(self.rtcqs, "power_management", "#quality_of_service_interface")
-        self.preferences_group.add(self.diagnostic_row)
+        self.cpu_group.add(self.diagnostic_row)
 
-        self.preferences_page.add(self.preferences_group)
+        # Kernel diagnostics
+
+        self.kernel_group = Adw.PreferencesGroup()
+        self.kernel_group.set_title("Kernel")
+
+        self.diagnostic_row = DiagnosticRow(self.rtcqs, "kernel_config")
+        self.kernel_group.add(self.diagnostic_row)
+
+        self.diagnostic_row = DiagnosticRow(self.rtcqs, "high_res_timers", "#installing_a_real-time_kernel")
+        self.kernel_group.add(self.diagnostic_row)
+
+        self.diagnostic_row = DiagnosticRow(self.rtcqs, "tickless", "#installing_a_real-time_kernel")
+        self.kernel_group.add(self.diagnostic_row)
+
+        self.diagnostic_row = DiagnosticRow(self.rtcqs, "preempt_rt", "#do_i_really_need_a_real-time_kernel")
+        self.kernel_group.add(self.diagnostic_row)
+
+        self.diagnostic_row = DiagnosticRow(self.rtcqs, "mitigations", "#disabling_spectre_and_meltdown_mitigations")
+        self.kernel_group.add(self.diagnostic_row)
+
+        # I/O diagnostics
+
+        self.io_group = Adw.PreferencesGroup()
+        self.io_group.set_title("I/O")
+
+        self.diagnostic_row = DiagnosticRow(self.rtcqs, "swappiness", "#sysctlconf")
+        self.io_group.add(self.diagnostic_row)
+
+        self.diagnostic_row = DiagnosticRow(self.rtcqs, "filesystems", "#filesystems")
+        self.io_group.add(self.diagnostic_row)
+
+        self.diagnostic_row = DiagnosticRow(self.rtcqs, "irqs")
+        self.io_group.add(self.diagnostic_row)
+
+        # self.preferences_page.add(self.explanations_group)
+        self.preferences_page.add(self.user_group)
+        self.preferences_page.add(self.cpu_group)
+        self.preferences_page.add(self.kernel_group)
+        self.preferences_page.add(self.io_group)
 
         self.main_box.append(self.preferences_page)
 
