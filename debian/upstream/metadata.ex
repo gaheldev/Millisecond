0f41cd0730420e51682b369c4c88367bbc2da957
Gahel <music@gahel.fr>
2025-05-14 17:47:20 +0200

    Improve deb info

--- debian/upstream/metadata.ex
+++ debian/upstream/metadata.ex
@@ -2,9 +2,9 @@
 # See https://wiki.debian.org/UpstreamMetadata for more info/fields.
 # Below an example based on a github project.
 
-# Bug-Database: https://github.com/<user>/millisecond/issues
-# Bug-Submit: https://github.com/<user>/millisecond/issues/new
-# Changelog: https://github.com/<user>/millisecond/blob/master/CHANGES
-# Documentation: https://github.com/<user>/millisecond/wiki
-# Repository-Browse: https://github.com/<user>/millisecond
-# Repository: https://github.com/<user>/millisecond.git
+Bug-Database: https://github.com/gaheldev/Millisecond/issues
+Bug-Submit: https://github.com/gaheldev/Millisecond/issues/new
+# Changelog: https://github.com/gaheldev/Millisecond/blob/master/CHANGES
+# Documentation: https://github.com/gaheldev/Millisecond/wiki
+Repository-Browse: https://github.com/gaheldev/Millisecond
+Repository: https://github.com/gaheldev/Millisecond.git
