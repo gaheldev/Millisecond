fc8b6fd40145ad2d6cb67560dd57b7dc786cac4d
Gahel <music@gahel.fr>
2025-05-21 02:46:49 +0200

    Fix copyright

--- src/window.py
+++ src/window.py
@@ -1,6 +1,6 @@
 # window.py
 #
-# Copyright 2025 Gaël
+# Copyright 2025 Gahel
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
